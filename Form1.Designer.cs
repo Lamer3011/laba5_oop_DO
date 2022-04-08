ff7f59f61588595c61930563013efe2e045f215f
Sergiy Kireev <48791896+Lamer3011@users.noreply.github.com>
2022-04-08 10:33:10 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace laba5_oop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.State = new System.Windows.Forms.Label();
+            this.Priority = new System.Windows.Forms.Label();
+            this.Id = new System.Windows.Forms.Label();
+            this.Alive = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.State);
+            this.groupBox1.Controls.Add(this.Priority);
+            this.groupBox1.Controls.Add(this.Id);
+            this.groupBox1.Controls.Add(this.Alive);
+            this.groupBox1.Controls.Add(this.Name);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 124);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CurrentThread";
+            // 
+            // State
+            // 
+            this.State.AutoSize = true;
+            this.State.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.State.Location = new System.Drawing.Point(152, 89);
+            this.State.Name = "State";
+            this.State.Size = new System.Drawing.Size(57, 16);
+            this.State.TabIndex = 5;
+            this.State.Text = "Running";
+            // 
+            // Priority
+            // 
+            this.Priority.AutoSize = true;
+            this.Priority.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Priority.Location = new System.Drawing.Point(152, 73);
+            this.Priority.Name = "Priority";
+            this.Priority.Size = new System.Drawing.Size(52, 16);
+            this.Priority.TabIndex = 4;
+            this.Priority.Text = "Normal";
+            // 
+            // Id
+            // 
+            this.Id.AutoSize = true;
+            this.Id.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Id.Location = new System.Drawing.Point(152, 57);
+            this.Id.Name = "Id";
+            this.Id.Size = new System.Drawing.Size(15, 16);
+            this.Id.TabIndex = 3;
+            this.Id.Text = "1";
+            // 
+            // Alive
+            // 
+            this.Alive.AutoSize = true;
+            this.Alive.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Alive.Location = new System.Drawing.Point(152, 41);
+            this.Alive.Name = "Alive";
+            this.Alive.Size = new System.Drawing.Size(36, 16);
+            this.Alive.TabIndex = 2;
+            this.Alive.Text = "True";
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name.Location = new System.Drawing.Point(152, 25);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(46, 16);
+            this.Name.TabIndex = 1;
+            this.Name.Text = "Form1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 80);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:\r\nIsAlive:\r\nManagedThreadId:\r\nPriority:\r\nThreadState:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start new Form";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop new Form";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 212);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label State;
+        private System.Windows.Forms.Label Priority;
+        private System.Windows.Forms.Label Id;
+        private System.Windows.Forms.Label Alive;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace laba5_oop
+{
+    public partial class Form1 : Form
+    {
+        List<Thread> myThreadsList = new List<Thread>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread currentThread = Thread.CurrentThread;
+            this.Text = "Form" + currentThread.ManagedThreadId.ToString();
+            currentThread.Name = this.Text;
+            Name.Text = currentThread.Name;
+            Alive.Text = currentThread.IsAlive.ToString();
+            Id.Text = currentThread.ManagedThreadId.ToString();
+            Priority.Text = currentThread.Priority.ToString();
+            State.Text = currentThread.ThreadState.ToString();
+        }
+
+        void show_new_Form()
+        {
+            Application.Run(new Form1());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread myThread = new Thread(show_new_Form);
+            myThread.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                label2.Text = ("Головний потік: " + i);
+                Thread.Sleep(200);
+            }
+
+            myThreadsList.Add(myThread);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (myThreadsList.Count == 0)
+            {
+                return;
+            }
+            Thread myThread = myThreadsList.Last();
+            myThread.Abort();
+            myThreadsList.Remove(myThread);
+        }
+    }
+}
+
